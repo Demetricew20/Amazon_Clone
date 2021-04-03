40b7656c28a6eeeb97665e0a433c671dc8c7ba16
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-03 15:40:00 -0800

    Built initial setup for application using create-react-app. Added config for firebase hosting in firebase.js

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAkmo9Lnl3Lfi960x7vXzt2bpV_8dybQoA",
+    authDomain: "clone-fbc7a.firebaseapp.com",
+    projectId: "clone-fbc7a",
+    storageBucket: "clone-fbc7a.appspot.com",
+    messagingSenderId: "252765527089",
+    appId: "1:252765527089:web:15d2ee472104b7bc5f8910",
+    measurementId: "G-T64MT4MFNQ"
+};
