9f54d968dfc2cf7d6a79dc4a8a9053cdedfab2fb
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-04 12:26:57 -0800

    Created new files for checkout page.

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -4,20 +4,20 @@ import Header from './Header';
 import Home from './Home';
 import {BrowserRouter as Router, Switch, Route }
 from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
   return (
     // BEM naming convention
     <Router>
     <div className="app">
+    <Header />
       <Switch>
       <Route path='/checkout'>
-          <Header />
-            <h1>Checkout</h1>
+          <Checkout />
         </Route>
         {/* Keep Default Route On the Bottom */}
         <Route path='/'>
-          <Header />
           <Home/>
         </Route>
 
