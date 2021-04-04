65f091077e87bfe3835fff57fc69268f6c419b19
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-04 09:15:25 -0800

    Added image for product. Styled Product and Home components

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -4,13 +4,19 @@ import './Product.css';
 function Product() {
     return (
         <div className='product'>
-            <div className='product_info'>
+            <div className='product__info'>
                 <p>Product Description</p>
                 <p className='product__price'>
                     <small>$</small>
                     <strong>19.99</strong>
                 </p>
+                <div class='product__rating'>
+                    <p>&#11088;</p>
+                </div>
             </div>
+
+            <img src='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg' alt='' />
+            <button>Add To Basket</button>
         </div>
     )
 }
