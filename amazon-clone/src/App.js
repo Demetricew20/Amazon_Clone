9e439748db37fe09dbd1f6e3e9d84640f84301f9
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-03 17:20:47 -0800

    Added search and shopping basket icons. Styled Header components.

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1> Amazon Store</h1>
       {/* Header */}
+      <Header />
       {/* Body - Home Component */}
     </div>
   );
