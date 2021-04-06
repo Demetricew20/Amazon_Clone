d5239dd4d2cc9b2782cfbcf2c946e953dfcb9062
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-05 19:05:57 -0800

    Added logic to 'Sign-In

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -29,9 +29,9 @@ function Header() {
             </div>
 
             <div className='header__nav'>
-            <Link to='/login'>
+            <Link to={!user && '/login'}>
                 <div onClick={handleAuthentication} className='header__options'>
-                    <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineOne'>Hello {auth.displayName}</span>
                     <span className='header__optionLineTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
                 </div>
             </Link>
