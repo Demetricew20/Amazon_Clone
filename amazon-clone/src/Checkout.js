5e1df9980bd23abc2be2e38f7b212fea729e7ef0
DemetriceW20 <demetrice_williams@hotmail.com>
2021-04-05 19:12:23 -0800

    Added logic to display user email in checkout if user is logged in. Will display guest if user has not logged in.

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -6,7 +6,7 @@ import Subtotal from './Subtotal'
 
 
 function Checkout() {
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
 
 
     return (
@@ -14,6 +14,7 @@ function Checkout() {
             <div className='checkout__left'>
                 <img className='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt=''/>
                 <div>
+                    <h3>Hello, {user ? user?.email : 'Guest' }</h3>
                     <h2 className='checkout__title'>Your Shopping Basket</h2>
 
                     {basket.map(item => (
@@ -28,10 +29,9 @@ function Checkout() {
 
                 </div>
             </div>
-
-            <div className='checkout__right'>
+            <h2 className='checkout__right'>
                 <Subtotal />
-            </div>
+            </h2>
         </div>
     )
 }
